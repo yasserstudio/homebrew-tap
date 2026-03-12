2838d63c361f593000b38f8de1dae18e80b71434
Yasser's studio <32455257+yasserstudio@users.noreply.github.com>
2026-03-12 16:54:30 +0100

    chore: bump gpc to v0.9.13

--- Formula/gpc.rb
+++ Formula/gpc.rb
@@ -4,24 +4,28 @@
 class Gpc < Formula
   desc "Ship Android apps from your terminal — Google Play Console CLI"
   homepage "https://github.com/yasserstudio/gpc"
-  version "0.1.4"
+  version "0.9.13"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/yasserstudio/gpc/releases/download/v#{version}/gpc-darwin-arm64"
-      sha256 "c82500e00fa017635de22ca04ceb7a36b74eb70bbaea7a33b953fe2a930bc98a"
+      sha256 "258dc00357d70931425dee8dcfb7f3f16682884271b8c0ca226bd38d7d30be10"
     end
     on_intel do
       url "https://github.com/yasserstudio/gpc/releases/download/v#{version}/gpc-darwin-x64"
-      sha256 "07440ca2276de3cb0b76db360b16ba1d850fd302b30d703cc2f23551e84b619e"
+      sha256 "847c34656b3fe4661f70466d2e9770be924b41d4042a28d15156b05618077a82"
     end
   end
 
   on_linux do
+    on_arm do
+      url "https://github.com/yasserstudio/gpc/releases/download/v#{version}/gpc-linux-arm64"
+      sha256 "069d0bbbd851dff82f1ec06bfb299b664d570f99727a0123524e104e16d335e0"
+    end
     on_intel do
       url "https://github.com/yasserstudio/gpc/releases/download/v#{version}/gpc-linux-x64"
-      sha256 "bcc8b3fcf8f2b4d979574520a7c43271c28dc52904b36ece7747c6f1b60f0ae5"
+      sha256 "50a40b56140d3a8eeff0d44a56a9f06434d5ab55cf96396b7e7a050b6031fdbc"
     end
   end
 
